e0a1d3027cefbbe142e2ba117fa47433d82daff1
Jennifer <jennifer15aries@gmail.com>
2024-07-28 14:48:17 -0500

    creating the classes

---
+++ js/class/Presupuesto.js
@@ -0,0 +1,29 @@
+//Clases
+class Presupuesto{
+    constructor(presupuesto){
+        this.presupuesto = Number(presupuesto);//monto total
+        this.restante = Number(presupuesto);//monto que disminuye segun la cantidad ingresada
+        this.gastos = []; //acumular los gastos
+    }
+
+    nuevoGasto(gasto){
+        //referencia al gasto
+        this.gastos = [...this.gastos,gasto];
+        this.calcularRestante();
+    }
+
+    calcularRestante(){
+        //Reduce -> Sumar los gastos
+        const gastado = this.gastos.reduce( (total,gasto) => total += gasto.cantidad, 0  )
+        this.restante = this.presupuesto - gastado;
+    }
+
+    eliminarGasto(id){
+        //Una vez eliminado el gasto
+        this.gastos = this.gastos.filter(gasto => gasto.id !== id);
+
+        //Llamamos el restante
+        this.calcularRestante();
+    }
+
+}
